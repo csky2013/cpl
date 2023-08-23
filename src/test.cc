d558817303e72babfaf0953cd3f2c394f054238e
caishu <caishu@bytedance.net>
2023-08-23 21:06:01 +0800

    init

---
+++ src/test.cc
@@ -0,0 +1,9 @@
+#include "test.h"
+
+namespace cpl {
+
+int add(int a, int b) {
+    return a + b;
+}
+
+}
---
+++ test/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << cpl::add(1, 2) << std::endl;
+    return 0;
+}
